f27ad7adf1b96182c6b7a6bf7223b99441b656c3
Forzoom <forzoomhm@gmail.com>
2015-02-02 17:31:21 +0800

    尝试wifip2p

--- MShare/src/org/mshare/p2p/P2pActivity.java
+++ MShare/src/org/mshare/p2p/P2pActivity.java
@@ -1,10 +1,19 @@
 package org.mshare.p2p;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import org.mshare.main.R;
 
 import android.app.Activity;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.os.Bundle;
@@ -15,6 +24,52 @@ public class P2pActivity extends Activity {
 	private Channel channel;
 	private P2pReceiver pr;
 	private IntentFilter filter;
+	
+	public static final String ACTION_ON_PEERS_AVAILABLE = "org.mshare.p2p.ON_PEERS_AVAILABLE";
+	
+	/**
+	 * 通知当peers可以使用的时候
+	 * @author HM
+	 *
+	 */
+	private class OnPeersAvailableReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Collection<WifiP2pDevice> devices = peers.getDeviceList();
+			Iterator<WifiP2pDevice> iterator = devices.iterator();
+			if (iterator.hasNext()) {
+				WifiP2pConfig config = new WifiP2pConfig();
+				WifiP2pDevice device = iterator.next();
+				config.deviceAddress = device.deviceAddress;
+				config.wps.setup = WpsInfo.PBC;
+				
+				wpm.connect(channel, config, new WifiP2pManager.ActionListener() {
+
+					@Override
+					public void onSuccess() {
+						
+					}
+
+					@Override
+					public void onFailure(int reason) {
+						
+					}
+					
+				});
+			}
+			
+			/*
+			Iterator<WifiP2pDevice> iterator = devices.iterator();
+			while (iterator.hasNext()) {
+				WifiP2pConfig config = new WifiP2pConfig();
+				WifiP2pDevice device = iterator.next();
+				config.deviceAddress = device.deviceAddress;
+				config.wps.setup = WpsInfo.PBC;
+			}
+			*/
+		}
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
--- MShare/src/org/mshare/p2p/P2pReceiver.java
+++ MShare/src/org/mshare/p2p/P2pReceiver.java
@@ -3,15 +3,22 @@ package org.mshare.p2p;
 import java.util.Collection;
 import java.util.Iterator;
 
+import org.mshare.main.MShareApp;
+
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.os.Bundle;
+import android.util.Log;
 
 /**
  * 接收WIFI_P2P_PEERS_CHANGE_ACTION
@@ -22,6 +29,7 @@ import android.net.wifi.p2p.WifiP2pManager.Channel;
  */
 public class P2pReceiver extends BroadcastReceiver {
 
+	private static final String TAG = P2pReceiver.class.getSimpleName();
 	private WifiP2pManager wpm;
 	private Channel channel;
 	private Activity activity;
@@ -42,6 +50,7 @@ public class P2pReceiver extends BroadcastReceiver {
 			
 			if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
 				// WIFIP2P可用
+				wpm.discoverPeers(channel, new OnDiscoverPeersListener());
 			} else if (state == WifiP2pManager.WIFI_P2P_STATE_DISABLED) {
 				// WIFIP2P不可用
 			}
@@ -50,55 +59,56 @@ public class P2pReceiver extends BroadcastReceiver {
 		// 检测当前peer列表发生变化的情况
 		// discoverPeers()被调用后返回的结果,requestPeers()来获得结果
 		if (action.equals(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)) {
-			wpm.discoverPeers(channel, new OnDiscoverPeersListener());
+			wpm.requestPeers(channel, new OnPeerListListener());
 		}
 
 	}
 	
-	private void error() {
-		
-	}
-	
-	private void busy() {
-		
-	}
-	
-	private void p2pUnsupported() {
-		
-	}
-	
+	/**
+	 * 当有新的Peer被发现的时候调用
+	 * @author HM
+	 *
+	 */
 	private class OnDiscoverPeersListener implements WifiP2pManager.ActionListener {
 
 		@Override
 		public void onSuccess() {
-			wpm.requestPeers(channel, new OnPeerListListener());
+			// 调用requestPeers放在了WIFI_P2P_PEERS的状态发生改变的时候
 		}
 
 		@Override
 		public void onFailure(int reason) {
 			switch (reason) {
 			case WifiP2pManager.ERROR:
-				error();
+				Log.e(TAG, "error");
 				break;
 			case WifiP2pManager.BUSY:
-				busy();
+				Log.e(TAG, "busy");
 				break;
 			case WifiP2pManager.P2P_UNSUPPORTED:
-				p2pUnsupported();
+				Log.e(TAG, "p2p_unsupported");
 				break;
 			}
 		}
 		
 	}
 	
+	/**
+	 * 当新的peer列表可以使用的时候
+	 * @author HM
+	 *
+	 */
 	private class OnPeerListListener implements WifiP2pManager.PeerListListener {
 
 		@Override
 		public void onPeersAvailable(WifiP2pDeviceList peers) {
 			// TODO 列出所有的Peer列表
 			
-			Collection<WifiP2pDevice> devices = peers.getDeviceList();
-			Iterator<WifiP2pDevice> iterator = devices.iterator();
+			Context context = MShareApp.getAppContext();
+			Intent intent = new Intent(P2pActivity.ACTION_ON_PEERS_AVAILABLE);
+			Bundle b = new Bundle();
+//			intent.put
+//			context.sendBroadcast(intent);
 		}
 		
 	}
