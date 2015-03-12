bac77a36bff98fd516c3b18cb7f0965de2e32fe6
Forzoom <forzoomhm@gmail.com>
2015-03-12 12:50:59 +0800

    完成基本的图片按钮功能

---
+++ MShare/src/org/mshare/picture/RefreshCallback.java
@@ -0,0 +1,14 @@
+package org.mshare.picture;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class RefreshCallback implements Handler.Callback {
+
+	@Override
+	public boolean handleMessage(Message msg) {
+		
+		return false;
+	}
+
+}
---
+++ MShare/src/org/mshare/picture/RefreshHandler.java
@@ -0,0 +1,12 @@
+package org.mshare.picture;
+
+import android.os.Handler;
+import android.os.Looper;
+
+public class RefreshHandler extends Handler {
+
+	public RefreshHandler(Looper looper, Handler.Callback callback) {
+		super(looper, callback);
+	}
+	
+}
